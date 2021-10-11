ec2792ff1215b3e065e8bf78d4d94cc5924019e9
Chitsanupong <first30952@gmail.com>
2021-10-11 14:03:09 +0700

    feat: output

---
+++ output.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Carlos"
+	var age uint = 21
+	// no white space & new line
+	fmt.Print("Hello")
+	fmt.Print("World")
+
+	// need to add own space
+	fmt.Print(name, " ", age) 
+	fmt.Print("\n")
+
+	// add white space & new line
+	fmt.Println(name, age)
+
+	// Printf
+	fmt.Printf("I'm %v and I'm %v years old", name, age)
+	fmt.Print("\n")
+	fmt.Printf("name's type: %T age's type: %T", name, age)
+}
