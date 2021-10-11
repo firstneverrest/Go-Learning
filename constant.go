512f719b8ad4e054208dfc4f49c4d51e6fe845d4
Chitsanupong <first30952@gmail.com>
2021-10-11 14:03:03 +0700

    feat: constant

---
+++ constant.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const studentNumber uint = 40
+
+const (
+	age int = 2
+	name = "Carlos"
+)
+
+func main() {
+	fmt.Println(studentNumber)
+	fmt.Println(age, name)
+}
